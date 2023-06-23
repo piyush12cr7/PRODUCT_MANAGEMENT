fea52edbe37b085f4c66f1d79b7412bd85d45bb2
Piyush <piyush12cr7@gmail.com>
2023-06-23 17:31:16 +0530

    FIRST COMMIT

---
+++ product_management/products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
